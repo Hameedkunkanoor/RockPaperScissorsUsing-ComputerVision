67cfb6371bf2e496c5a63c53c2d80964c99dcb49
Hameedkunkanoor <41198503+Hameedkunkanoor@users.noreply.github.com>
2020-02-25 09:10:54 +0530

    Added mo9del builder python file

---
+++ ModelBuilder.py
@@ -0,0 +1,66 @@
+import os
+from keras.preprocessing.image import ImageDataGenerator
+training_dir='Data'
+validation_dir='Validation'
+training_rock_dir='Data\Rock'
+training_paper_dir='Data\Paper'
+training_scissors_dir='Data\Scissors'
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+print('The total training images of rock is',len(os.listdir(training_rock_dir)))
+
+print('The total training images of paper is',len(os.listdir(training_paper_dir)))
+
+print('The total training images of scissors is',len(os.listdir(training_scissors_dir)))
+
+from keras import layers
+from keras import models
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1))
+from keras import optimizers
+import keras
+model.compile(loss='binary_crossentropy',
+optimizer=keras.optimizers.Adam(),
+metrics=['acc'])
+
+train_generator = train_datagen.flow_from_directory(
+training_dir,
+target_size=(150, 150),
+batch_size=20,
+class_mode='binary')
+validation_generator=test_datagen.flow_from_directory(validation_dir,target_size=(150,150),
+batch_size=20,class_mode='binary')
+history = model.fit_generator(
+train_generator,
+steps_per_epoch=100,
+epochs=5,
+validation_data=validation_generator,
+validation_steps=50)
+model.save('RockPaperScissors.h5')
+import matplotlib.pyplot as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
